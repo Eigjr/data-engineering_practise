c476fae0e8d10dcf9c164296a8abf5f4885f8cae
ema <emaigbo1@gmail.com>
2025-02-28 17:13:24 +0100

    week one
     a details of week_one assignment

---
+++ week_one_code.py
@@ -0,0 +1,21 @@
+"""score_sheet = {
+'john' : 40,
+'peter': 50,
+'jerry': 70,
+'paul': 80,
+'jame': 40}
+
+pass_mark = 60
+num_student = 5
+
+# def student_score():
+for score in score_sheet:
+    if int(score) < pass_mark:
+        print('Oh!, you didn\'t  mke the cut up mark')"""
+
+second = 60
+
+def days_in_second(day):
+    print (f'enter a number and i\'ll convert to its equivalent in seconds {second * day} ')
+
+days_in_second(3)
